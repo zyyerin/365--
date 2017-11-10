f43ccee282ac1218e66d8093653b54c93942155c
Erin Zhang <zyyerin@gmail.com>
2017-11-10 10:02:58 -0800

    214

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+const scribble = new Scribble();              // global mode
+
 let num;
 let intervalX;
 let bubbleR;
@@ -8,56 +10,41 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   stroke(255);
-  frameRate(24);
+  frameRate(48);
 
   // initializing variable
   num = 0;
-  bubbleR = 200;
-  numLimit = random(10, 20);
+  bubbleR = height/6;
+  numLimit = 5;
 }
 
 function draw() {
-  background(0);
-  let sw = intervalX/3;
+  // background(0);
+  fill(0, 100);
+  rect(0, 0, width, height);
+  noFill();
+  let sw = intervalX/4;
   intervalX = bubbleR/num;
-  let offset = frameCount*0.07;
-
+  let offset = 0;
 
   if (num < numLimit){ num ++; }
 
   strokeWeight(sw);
 
   push();
-  translate(width/2, height/2);
-  rotate(map(mouseY, 0, width, 0, PI));
-  for(let i=0; i<num; i++) {
-    line(i*intervalX, 0, i*intervalX-offset, sqrt(sq(bubbleR)-sq(i*intervalX)));
+  translate(mouseX, mouseY);
+  rotate(map(mouseY, 0, height, 0, PI));
+  for(let i=-num; i<num; i++) {
+    scribble.scribbleLine(i*intervalX, sqrt(sq(bubbleR)-sq(i*intervalX)), i*intervalX-offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
   }
-  for(let i=0; i<num; i++) {
-    line(i*intervalX, 0, i*intervalX-offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
-  for(let i=0; i<num; i++) {
-    line(-i*intervalX, 0, -i*intervalX-offset, sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
-  for(let i=0; i<num; i++) {
-    line(-i*intervalX, 0, -i*intervalX-offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
-
-
   rotate(map(mouseX, 0, width, 0, PI));
-  for(let i=0; i<num; i++) {
-    line(i*intervalX, 0, i*intervalX+offset, sqrt(sq(bubbleR)-sq(i*intervalX)));
+  for(let i=-num; i<num; i++) {
+    scribble.scribbleLine(i*intervalX, sqrt(sq(bubbleR)-sq(i*intervalX)), i*intervalX-offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
   }
-  for(let i=0; i<num; i++) {
-    line(i*intervalX, 0, i*intervalX+offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
-  for(let i=0; i<num; i++) {
-    line(-i*intervalX, 0, -i*intervalX+offset, sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
-  for(let i=0; i<num; i++) {
-    line(-i*intervalX, 0, -i*intervalX+offset, -sqrt(sq(bubbleR)-sq(i*intervalX)));
-  }
+
   pop();
+}
 
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
