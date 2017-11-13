437a326099ddbd10909c1aa7075fa88614635742
Erin Zhang <zyyerin@gmail.com>
2017-11-12 18:32:39 -0800

    212--

--- sketch.js
+++ sketch.js
@@ -5,32 +5,56 @@ let angle;
 
 //p5 lerp
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(125);
   angleMode(DEGREES);
   mic = new p5.AudioIn();
   mic.start();
   amp = new p5.Amplitude();
 
-  angle = 360;
+  angle = 0;
 }
 
 function draw() {
-  //    background(255);
-  fill(255, 20);
-  noStroke();
-  rect(0, 0, width, height);
+  background(255);
+
   let vol = mic.getLevel();
+  print(vol);
+  let detail = floor(map(vol, 0, 1, height/5, width));
+
   volhistory.push(vol);
 
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
   }
 
-  stroke(0);
-  strokeWeight(vol*100);
-  noFill();
-  ellipse(random(width), random(height), vol * height*2, vol * height*2);
+
+  push();
+  rotateY(angle);
+  rotateZ(-angle);
+
+
+  translate(-height/10, 0);
   fill(0);
-  ellipse(random(width), random(height), vol * width*2, vol * width*2);
+  box(height/10, height/5, detail);
+
+  translate(height/10, 0);
+  fill(200);
+  box(height/10, height/5, detail);
+  pop();
+  angle += 0.005;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
