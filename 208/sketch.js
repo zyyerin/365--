18b30689a1d7bc3bd85822454f61ec4f3abd86eb
Erin Zhang <zyyerin@gmail.com>
2017-11-16 23:34:26 -0800

    208-fix
    update wrong file

--- 208/sketch.js
+++ 208/sketch.js
@@ -1,5 +1,8 @@
 let para;
-let s;
+let s, rs, pos;
+let words;
+let nnNum = 0;
+
 const LEXICON = new RiLexicon();
 
 function preload(){
@@ -7,59 +10,66 @@ function preload(){
 }
 
 function setup() {
-    noCanvas();
-    processRita();
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  processRita();
+}
+
+function draw() {
+  background(255);
+  for (let i=0; i<words.length; i++) {
+    text(words[i], noise(frameCount/((i+1)*10))*width, noise(frameCount/((i+1)*i))*height);
+  }
 }
 
 function processRita() {
-  // let s = input.value();
+  rs = new RiString(s[0]);
+  words = rs.words();
+  pos = rs.pos();
 
-  print(s);
-
-  let rs = new RiString(s[0]);
-  let words = rs.words();
-  let pos = rs.pos();
-
-  for (let i=0; i<words.length; i++) {
-    switch(pos[i]){
-      case ',':
-      case ';':
-      case '.':
-      createDiv('').addClass('end');
-      break;
-      case 'cc':
-        createDiv('').addClass('cc tri');
-        break;
-      case 'cd':
-        createDiv('').addClass('cd tri');
-        break;
-      case 'nn':
-      case 'nns':
-        createDiv('').addClass('nn');
-        break;
-      case 'nnp':
-      case 'nnps':
-        createDiv('').addClass('nnp');
-        break;
-      case 'vb':
-      case 'vbd':
-      case 'vbg':
-      case 'vbn':
-      case 'vbp':
-      case 'vbz':
-        createDiv('').addClass('vb');
-        break;
-      case 'prp':
-        createDiv('').addClass('prp');
-        break;
-      case 'jj':
-      case 'jjr':
-      case 'jjs':
-        createDiv('').addClass('jj');
-        break;
-      default:
-        print(pos[i]);
-        createDiv('').addClass('not');
-    }
-  }
+  // for (let i=0; i<words.length; i++) {
+  //   switch(pos[i]){
+  //     case ',':
+  //     case ';':
+  //     case '.':
+  //     createDiv('').addClass('end');
+  //     break;
+  //     case 'cc':
+  //       createDiv('').addClass('cc tri');
+  //       break;
+  //     case 'cd':
+  //       createDiv('').addClass('cd tri');
+  //       break;
+  //     case 'nn':
+  //     case 'nns':
+  //       createDiv('').addClass('nn');
+  //       nnNum ++;
+  //       print(nnNum);
+  //
+  //       break;
+  //     case 'nnp':
+  //     case 'nnps':
+  //       createDiv('').addClass('nnp');
+  //       break;
+  //     case 'vb':
+  //     case 'vbd':
+  //     case 'vbg':
+  //     case 'vbn':
+  //     case 'vbp':
+  //     case 'vbz':
+  //       createDiv('').addClass('vb');
+  //       break;
+  //     case 'prp':
+  //       createDiv('').addClass('prp');
+  //       break;
+  //     case 'jj':
+  //     case 'jjr':
+  //     case 'jjs':
+  //       createDiv('').addClass('jj');
+  //       break;
+  //     default:
+  //       print(pos[i]);
+  //       createDiv('').addClass('not');
+  //   }
+  // }
 }
